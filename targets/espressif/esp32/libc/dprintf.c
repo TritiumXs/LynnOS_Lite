2b67042a1fbeac4f9733aed9a949bab517b3f6cb
zhaoxc0502 <zhaoxc0502@thundersoft.com>
2021-12-29 10:20:51 +0800

    merge vim3l content
    Signed-off-by: zhaoxc0502 <zhaoxc0502@thundersoft.com>

--- targets/espressif/esp32/libc/dprintf.c
+++ targets/espressif/esp32/libc/dprintf.c
@@ -17,108 +17,107 @@
 #include <stdio.h>
 #include "los_debug.h"
 
-static int hex2asc(int n)
-{
+static int hex2asc(int n) {
     n &= 15;
-    if(n > 9){
+    if (n > 9) {
         return ('a' - 10) + n;
     } else {
         return '0' + n;
     }
 }
 
-static void dputs(char const *s, int (*pFputc)(int n, void *cookie), void *cookie)
-{
+static void dputs(char const *s, int (*pFputc)(int n, void *cookie), void *cookie) {
     while (*s) {
         pFputc(*s++, cookie);
     }
 }
 
-void __dprintf(char const *fmt, va_list ap,
-        int (*pFputc)(int n, void *cookie),
-        void *cookie)
-{
+void __dprintf(char const *fmt, va_list ap, int (*pFputc)(int n, void *cookie), void *cookie) {
     char scratch[256];
 
-    for(;;){
-        switch(*fmt){
+    for (;;) {
+        switch (*fmt) {
             case 0:
                 va_end(ap);
                 return;
             case '%':
-                switch(fmt[1]) {
+                switch (fmt[1]) {
                     case 'c': {
-                                  unsigned n = va_arg(ap, unsigned);
-                                  pFputc(n, cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        unsigned n = va_arg(ap, unsigned);
+                        pFputc(n, cookie);
+                        fmt += 2;
+                        continue;
+                    }
                     case 'h': {
-                                  unsigned n = va_arg(ap, unsigned);
-                                  pFputc(hex2asc(n >> 12), cookie);
-                                  pFputc(hex2asc(n >> 8), cookie);
-                                  pFputc(hex2asc(n >> 4), cookie);
-                                  pFputc(hex2asc(n >> 0), cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        unsigned n = va_arg(ap, unsigned);
+                        pFputc(hex2asc(n >> 12), cookie);
+                        pFputc(hex2asc(n >> 8), cookie);
+                        pFputc(hex2asc(n >> 4), cookie);
+                        pFputc(hex2asc(n >> 0), cookie);
+                        fmt += 2;
+                        continue;
+                    }
                     case 'b': {
-                                  unsigned n = va_arg(ap, unsigned);
-                                  pFputc(hex2asc(n >> 4), cookie);
-                                  pFputc(hex2asc(n >> 0), cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        unsigned n = va_arg(ap, unsigned);
+                        pFputc(hex2asc(n >> 4), cookie);
+                        pFputc(hex2asc(n >> 0), cookie);
+                        fmt += 2;
+                        continue;
+                    }
                     case 'p':
                     case 'X':
                     case 'x': {
-                                  unsigned n = va_arg(ap, unsigned);
-                                  char *p = scratch + 15;
-                                  *p = 0;
-                                  do {
-                                      *--p = hex2asc(n);
-                                      n = n >> 4;
-                                  } while(n != 0);
-                                  while(p > (scratch + 7)) *--p = '0';
-                                  dputs(p, pFputc, cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        unsigned n = va_arg(ap, unsigned);
+                        char *p = scratch + 15;
+                        *p = 0;
+                        do {
+                            *--p = hex2asc(n);
+                            n = n >> 4;
+                        } while (n != 0);
+                        while (p > (scratch + 7)) {
+                            *--p = '0';
+                        }
+                        dputs(p, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
                     case 'd': {
-                                  int n = va_arg(ap, int);
-                                  char *p = scratch + 15;
-                                  *p = 0;
-                                  if(n < 0) {
-                                      pFputc('-', cookie);
-                                      n = -n;
-                                  }
-                                  do {
-                                      *--p = (n % 10) + '0';
-                                      n /= 10;
-                                  } while(n != 0);
-                                  dputs(p, pFputc, cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        int n = va_arg(ap, int);
+                        char *p = scratch + 15;
+                        *p = 0;
+                        if (n < 0) {
+                            pFputc('-', cookie);
+                            n = -n;
+                        }
+                        do {
+                            *--p = (n % 10) + '0';
+                            n /= 10;
+                        } while (n != 0);
+                        dputs(p, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
                     case 'u': {
-                                  unsigned int n = va_arg(ap, unsigned int);
-                                  char *p = scratch + 15;
-                                  *p = 0;
-                                  do {
-                                      *--p = (n % 10) + '0';
-                                      n /= 10;
-                                  } while(n != 0);
-                                  dputs(p, pFputc, cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        unsigned int n = va_arg(ap, unsigned int);
+                        char *p = scratch + 15;
+                        *p = 0;
+                        do {
+                            *--p = (n % 10) + '0';
+                            n /= 10;
+                        } while (n != 0);
+                        dputs(p, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
                     case 's': {
-                                  char *s = va_arg(ap, char*); /*lint !e64*/
-                                  if(s == 0) s = "(null)";
-                                  dputs(s, pFputc, cookie);
-                                  fmt += 2;
-                                  continue;
-                              }
+                        char *s = va_arg(ap, char*); /*lint !e64*/
+                        if (s == 0) {
+                            s = "(null)";
+                        }
+                        dputs(s, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
                 }
                 pFputc(*fmt++, cookie);
                 break;
@@ -129,8 +128,7 @@ void __dprintf(char const *fmt, va_list ap,
         }
     }
 }
-int hal_trace_printf(int attr, const char *fmt, ...)
-{
+int hal_trace_printf(int attr, const char *fmt, ...) {
     va_list arg;
 
     va_start(arg, fmt);
