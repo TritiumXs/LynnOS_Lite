81904eebd2043748b5d84cdd1a2a8b275e0b8dfc
zhaoxc0502 <zhaoxc0502@thundersoft.com>
2022-01-20 00:00:00 +0800

    modify for add nxp && vim3l && esp32c3 board
    Signed-off-by: zhaoxc0502 <zhaoxc0502@thundersoft.com>

---
+++ targets/espressif/esp32/libc/dprintf.c
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2020-2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "stdarg.h"
+#include <stdio.h>
+#include "los_debug.h"
+
+static int hex2asc(int n) {
+    n &= 15;
+    if (n > 9) {
+        return ('a' - 10) + n;
+    } else {
+        return '0' + n;
+    }
+}
+
+static void dputs(char const *s, int (*pFputc)(int n, void *cookie), void *cookie) {
+    while (*s) {
+        pFputc(*s++, cookie);
+    }
+}
+
+void __dprintf(char const *fmt, va_list ap, int (*pFputc)(int n, void *cookie), void *cookie) {
+    char scratch[256];
+
+    for (;;) {
+        switch (*fmt) {
+            case 0:
+                va_end(ap);
+                return;
+            case '%':
+                switch (fmt[1]) {
+                    case 'c': {
+                        unsigned n = va_arg(ap, unsigned);
+                        pFputc(n, cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                    case 'h': {
+                        unsigned n = va_arg(ap, unsigned);
+                        pFputc(hex2asc(n >> 12), cookie);
+                        pFputc(hex2asc(n >> 8), cookie);
+                        pFputc(hex2asc(n >> 4), cookie);
+                        pFputc(hex2asc(n >> 0), cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                    case 'b': {
+                        unsigned n = va_arg(ap, unsigned);
+                        pFputc(hex2asc(n >> 4), cookie);
+                        pFputc(hex2asc(n >> 0), cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                    case 'p':
+                    case 'X':
+                    case 'x': {
+                        unsigned n = va_arg(ap, unsigned);
+                        char *p = scratch + 15;
+                        *p = 0;
+                        do {
+                            *--p = hex2asc(n);
+                            n = n >> 4;
+                        } while (n != 0);
+                        while (p > (scratch + 7)) {
+                            *--p = '0';
+                        }
+                        dputs(p, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                    case 'd': {
+                        int n = va_arg(ap, int);
+                        char *p = scratch + 15;
+                        *p = 0;
+                        if (n < 0) {
+                            pFputc('-', cookie);
+                            n = -n;
+                        }
+                        do {
+                            *--p = (n % 10) + '0';
+                            n /= 10;
+                        } while (n != 0);
+                        dputs(p, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                    case 'u': {
+                        unsigned int n = va_arg(ap, unsigned int);
+                        char *p = scratch + 15;
+                        *p = 0;
+                        do {
+                            *--p = (n % 10) + '0';
+                            n /= 10;
+                        } while (n != 0);
+                        dputs(p, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                    case 's': {
+                        char *s = va_arg(ap, char*); /*lint !e64*/
+                        if (s == 0) {
+                            s = "(null)";
+                        }
+                        dputs(s, pFputc, cookie);
+                        fmt += 2;
+                        continue;
+                    }
+                }
+                pFputc(*fmt++, cookie);
+                break;
+            case '\n':
+                pFputc('\r', cookie);
+            default: /*lint !e616*/
+                pFputc(*fmt++, cookie);
+        }
+    }
+}
+int hal_trace_printf(int attr, const char *fmt, ...) {
+    va_list arg;
+
+    va_start(arg, fmt);
+    vprintf(fmt, arg);
+    va_end(arg);
+
+    return 0;
+}
+
+int vsnprintf_s(char *strDest, size_t destMax, size_t count, const char *format, va_list argList)
+{
+    int retVal;
+
+    if (destMax > count) {
+        retVal = SecVsnprintfImpl(strDest, count + 1, format, argList);
+    } else {
+        retVal = SecVsnprintfImpl(strDest, destMax, format, argList);
+    }
+
+    return retVal;
+}
+
+int snprintf_s(char *strDest, size_t destMax, size_t count, const char *format, ...)
+{
+    int ret;                    /* If initialization causes  e838 */
+    va_list argList;
+
+    va_start(argList, format);
+    ret = vsnprintf_s(strDest, destMax, count, format, argList);
+    va_end(argList);
+    (void)argList;              /* To clear e438 last value assigned not used , the compiler will optimize this code */
+
+    return ret;
+}
