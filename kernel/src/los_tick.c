6bca8666488d6bf391874b02bf9f0085a1689d83
zhushengle <zhushengle@huawei.com>
2022-02-22 18:28:43 +0800

    fix: 修复tick timer补偿问题
    Signed-off-by: zhushengle <zhushengle@huawei.com>
    Change-Id: I9025a88219f3dc2607de628979dbad8a66e0933d

--- kernel/src/los_tick.c
+++ kernel/src/los_tick.c
@@ -48,13 +48,17 @@ LITE_OS_SEC_BSS STATIC UINT64 g_tickTimerStartTime;
 
 #if (LOSCFG_BASE_CORE_TICK_WTIMER == 0)
 STATIC UINT64 g_tickTimerBase;
+STATIC BOOL g_tickTimerBaseUpdate = FALSE;
 
 LITE_OS_SEC_TEXT STATIC VOID OsUpdateSysTimeBase(VOID)
 {
     UINT32 period = 0;
 
-    (VOID)g_sysTickTimer->getCycle(&period);
-    g_tickTimerBase += period;
+    if (g_tickTimerBaseUpdate == FALSE) {
+        (VOID)g_sysTickTimer->getCycle(&period);
+        g_tickTimerBase += period;
+    }
+    g_tickTimerBaseUpdate = FALSE;
 }
 
 LITE_OS_SEC_TEXT VOID OsTickTimerBaseReset(UINT64 currTime)
@@ -96,6 +100,7 @@ LITE_OS_SEC_TEXT UINT64 LOS_SysCycleGet(VOID)
         /* Turn the timer count */
         g_tickTimerBase += period;
         schedTime = g_tickTimerBase + time;
+        g_tickTimerBaseUpdate = TRUE;
     }
 
     LOS_ASSERT(schedTime >= oldSchedTime);
