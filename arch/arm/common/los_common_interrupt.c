c6f31a5bdc9c096a5bd1110760fb89a364c72a58
zhangkt <zhangkt1995@gmail.com>
2023-11-23 09:52:15 +0800

    fix:修复设置LOSCFG_PLATFORM_HWI_WITH_ARG为1后，编译失败

--- arch/arm/common/los_common_interrupt.c
+++ arch/arm/common/los_common_interrupt.c
@@ -105,11 +105,6 @@ BOOL OsHwiIsCreated(UINT32 index)
 #endif
 
 #if (LOSCFG_PLATFORM_HWI_WITH_ARG == 1)
-typedef struct {
-    HWI_PROC_FUNC pfnHandler;
-    VOID *pParm;
-} HWI_HANDLER_FUNC;
-
 /* *
  * @ingroup los_hwi
  * Hardware interrupt handler form mapping handling function array.
